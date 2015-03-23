2894b98101ec8e771ee85e97fab0db544e3e7d42
cimmanon <chris@cimmanon.org>
2015-03-23 19:52:26 -0400

    Made CssType a GADT

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, GADTs #-}
 
 module Types
 	( module Types.Color
@@ -13,15 +13,29 @@ import Types.Color
 
 data ListDelimiter = Comma | Space
 
-data CString = CString
-	{ string :: String
-	, quote :: Maybe Char
-	}
+data CssType a where
+	CssString :: String -> CssType String
+	CssColor :: Color -> CssType Color
+	CssLength :: Length -> CssType Length
+	CssList :: ([CssType a], ListDelimiter) -> CssType ([CssType a], ListDelimiter)
+	CssMapping :: [(String, CssType a)] -> CssType [(String, CssType a)]
+	-- ^ the "keys" for mappings need to be restricted to String to avoid confusion when using Color names (eg. red)
+
+-- we have to make our own instances for GADTs, we can't just derive it like normal
+-- TODO: make nicer show instances
+instance (Show a) => Show (CssType a) where
+	show (CssString s) = show s
+	show (CssColor c) = show c
+	show (CssLength l) = show l
+	show (CssList xs) = show xs
+	show (CssMapping xs) = show xs
+
 {-
-data CList = CList
-	{ values :: [Value]
-	, delimiter :: ListDelimiter
-	}
+lighten :: CssType Color -> CssType Color
+lighten = id
+
+lighten' :: CssType Color -> Color
+lighten' (CssColor c) = c
 -}
 
 {----------------------------------------------------------------------------------------------------{
