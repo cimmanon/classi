266d68c83a375b7ef899cbf9fa5b1f3448f117c7
cimmanon <chris@cimmanon.org>
2016-03-09 10:24:46 -0500

    Added placeholder for calc() expressions

--- src/Types.hs
+++ src/Types.hs
@@ -9,15 +9,18 @@ module Types
 import Types.Color
 import Types.Length
 
--- Going to want to have a mapping type here as well, but that can come later
---data Value = Color | Length | Int | CString | CList
-
 data ListDelimiter = Comma | Space
+
+data Expression = Expression [String]
 
 data CssType a where
 	CssString :: String -> CssType String
 	CssColor :: Color -> CssType Color
 	CssLength :: Length -> CssType Length
+	CssCalc :: Expression -> CssType Expression
+	-- ^ TODO: figure out how to actually handle calc() expressions
+	-- CssCalc is going to need an instance of Num and Fractal
+	-- https://www.w3.org/TR/css3-values/#calc-notation
 	CssList :: ([CssType a], ListDelimiter) -> CssType ([CssType a], ListDelimiter)
 	CssMapping :: [(String, CssType a)] -> CssType [(String, CssType a)]
 	-- ^ the "keys" for mappings need to be restricted to String to avoid confusion when using Color names (eg. red)
@@ -28,6 +31,7 @@ instance (Show a) => Show (CssType a) where
 	show (CssString s) = show s
 	show (CssColor c) = show c
 	show (CssLength l) = show l
+	show (CssCalc e) = show e
 	show (CssList xs) = show xs
 	show (CssMapping xs) = show xs
 
